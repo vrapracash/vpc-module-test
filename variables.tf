27d78ff5347eab0884b2a2f3c2a8c2c3d061b5ac
veeraprakash <veeraprakash.bg@gmail.com>
2025-02-06 19:25:28 +0530

    script-update

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  type        = string
+  default     = "expense"
+}
+variable "environment" {
+    default = "dev"
+}
+variable "vpc_cidr" {
+    default = 10.0.0.0/16
+}
+variable "common_tags" {
+    default = {
+        Project = "ecpense"
+        Terraform = true
+        Environment = "dev"
+    }
+}
+variable "public_subnet_cidrs" {
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+variable "private_subnet_cidrs" {
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+variable "database_subnet_cidrs" {
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+variable "is_peering_required" {
+    default = true
+}
---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+    #source = "../terraform_aws_vpc"
+    source = https://github.com/vrapracash/terraform-aws-vpc.git
+    vpc_cidr = var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    common_tags = var.common_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+    is_peering_required = var.is_peering_required
+
+}
