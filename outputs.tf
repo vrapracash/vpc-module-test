894c38fd1b568e4522119d64bcfb51c6aa78ce39
vrapracash <veeraprakash.bg@gmail.com>
2025-02-07 16:28:01 +0530

    script-update

--- outputs.tf
+++ outputs.tf
@@ -10,14 +10,14 @@ output "public_subnet_ids" {
     value = module.vpc.public_subnet_ids
 }
 
-output "az_info" {
-    value = module.vpc.az_info
-}
+# output "az_info" {
+#     value = module.vpc.az_info
+# }
 
-output "default_vpc_info" {
-    value = module.vpc.default_vpc_info
-}
+# output "default_vpc_info" {
+#     value = module.vpc.default_vpc_info
+# }
 
-output "main_route_table_info" {
-    value = module.vpc.main_route_table_info
-}
+# output "main_route_table_info" {
+#     value = module.vpc.main_route_table_info
+# }
